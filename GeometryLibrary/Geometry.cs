70722dec9edbd371f82dc96f10933ceaf61090f4
MTR\yakushevich.io <yakushevich.io@medotrade.ru>
2023-07-20 18:56:29 +0300

    Добавьте файлы проекта.

---
+++ GeometryLibrary/Geometry.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace GeometryLibrary
+{
+    public static class Geometry
+    {
+        public static double GetCircleArea(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("Радиус не может быть отрицательным");
+            }
+            return Math.PI * radius * radius;
+        }
+
+        public static double GetRectangleArea(double side1, double side2)
+        {
+            if (side1 < 0 || side2 < 0)
+            {
+                throw new ArgumentException("Стороны прямоугольника не могут быть отрицательными");
+            }
+            return side1 * side2;
+        }
+
+        public static double GetTriangleArea(double side1, double side2, double side3)
+        {
+            if (side1 < 0 || side2 < 0 || side3 < 0)
+            {
+                throw new ArgumentException("Стороны треугольника не могут быть отрицательными");
+            }
+            double semiPerimeter = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+        }
+
+        public static double GetArea(double[] sides)
+        {
+            // если передана одна сторона считаем что это круг и передан только радиус
+            if (sides.Length == 1)
+            {
+                return GetCircleArea(sides[0]);
+            }
+            // если переданы две стороны это прямоугольник
+            else if (sides.Length == 2)
+            {
+                double length  = sides[0];
+                double width = sides[1];
+
+                //проверяем не квадрат ли входящая фигура
+                if (length == width)
+                {
+                    return length * length;
+                }
+
+                else
+                {
+                    return GetRectangleArea(width, length);
+                }
+            }
+
+            else if (sides.Length == 3)
+            {
+                double side1 = sides[0];
+                double side2 = sides[1];
+                double side3 = sides[2];
+
+                // проверяем треугольник на условие a²+ b²=c² для понимания прямоугольный ли он
+                if (side1 * side1 + side2 * side2 == side3 * side3 || side1 * side1 + side3 * side3 == side2 * side2 || side2 * side2 + side3 * side3 == side1 * side1)
+                {                           
+                    return (side1 * side2) / 2;
+                }
+                else
+                {
+                    // the triangle is not right-angled
+                    return GetTriangleArea(side1, side2, side3);
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Для других фигур методы не написаны, возвращайтесь к нам позже =)");
+            }
+        }
+    }
+}
---
+++ GeometryLibrary/Test/TestGeometry.cs
@@ -0,0 +1,103 @@
+﻿using GeometryLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeometryLibraryTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void GetCircleArea_PositiveRadius_ReturnsCorrectArea()
+        {
+            double radius = 5;
+            double expectedArea = 78.53981633974483;
+
+            double actualArea = Geometry.GetCircleArea(radius);
+
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetCircleArea_NegativeRadius_ThrowsArgumentException()
+        {
+
+            double radius = -5;
+
+            double actualArea = Geometry.GetCircleArea(radius);
+        }
+
+
+        [TestMethod]
+        public void GetTriangleArea_PositiveSides_ReturnsCorrectArea()
+        {
+
+            double side1 = 3;
+            double side2 = 4;
+            double side3 = 5;
+            double expectedArea = 6;
+
+            double actualArea = Geometry.GetTriangleArea(side1, side2, side3);
+
+
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetTriangleArea_NegativeSides_ThrowsArgumentException()
+        {
+            double side1 = -3;
+            double side2 = 4;
+            double side3 = 5;
+
+            double actualArea = Geometry.GetTriangleArea(side1, side2, side3);
+        }
+
+        [TestMethod]
+        public void GetArea_Circle_ReturnsCorrectArea()
+        {
+            double[] sides = new double[] { 5 };
+            double expectedArea = 78.53981633974483;
+
+            double actualArea = Geometry.GetArea(sides);
+
+
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void GetArea_Triangle_ReturnsCorrectArea()
+        {
+            double[] sides = new double[] { 3, 4, 5 };
+            double expectedArea = 6;
+
+            double actualArea = Geometry.GetArea(sides);
+
+
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void GetArea_RightAngledTriangle_ReturnsCorrectArea()
+        {
+            double[] sides = new double[] { 3, 4, 5 };
+            double expectedArea = 6;
+
+            double actualArea = Geometry.GetArea(sides);
+
+
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetArea_InvalidNumberOfSides_ThrowsArgumentException()
+        {
+            double[] sides = new double[] { 3, 4 };
+
+            double actualArea = Geometry.GetArea(sides);
+        }
+    }
+}
